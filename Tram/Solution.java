68d21a90c85acad2e8c6334e34005fb7c0f43e5f
Swathi Penmatsa <74686794+swathipenmatsa19@users.noreply.github.com>
2020-11-20 14:36:14 -0600

    116A (implementation)

---
+++ Tram/Solution.java
@@ -0,0 +1,67 @@
+/*
+
+A. Tram
+time limit per test2 seconds
+memory limit per test256 megabytes
+inputstandard input
+outputstandard output
+Linear Kingdom has exactly one tram line. It has n stops, numbered from 1 to n in the order of tram's movement. At the i-th stop ai passengers exit the tram, while bi passengers enter it. The tram is empty before it arrives at the first stop. Also, when the tram arrives at the last stop, all passengers exit so that it becomes empty.
+
+Your task is to calculate the tram's minimum capacity such that the number of people inside the tram at any time never exceeds this capacity. Note that at each stop all exiting passengers exit before any entering passenger enters the tram.
+
+Input
+The first line contains a single number n (2 ≤ n ≤ 1000) — the number of the tram's stops.
+
+Then n lines follow, each contains two integers ai and bi (0 ≤ ai, bi ≤ 1000) — the number of passengers that exits the tram at the i-th stop, and the number of passengers that enter the tram at the i-th stop. The stops are given from the first to the last stop in the order of tram's movement.
+
+The number of people who exit at a given stop does not exceed the total number of people in the tram immediately before it arrives at the stop. More formally, . This particularly means that a1 = 0.
+At the last stop, all the passengers exit the tram and it becomes empty. More formally, .
+No passenger will enter the train at the last stop. That is, bn = 0.
+Output
+Print a single integer denoting the minimum possible capacity of the tram (0 is allowed).
+
+Examples
+input
+4
+0 3
+2 5
+4 2
+4 0
+output
+6
+
+*/
+
+
+import java.io.*;
+import java.util.*;
+ 
+ 
+public class Solution {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int max = 0;
+        int sum = 0;
+        for(int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+                sum = sum - a;
+            sum += b;
+            if(sum > max)
+                max = sum;
+        }
+        System.out.println(max);
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
